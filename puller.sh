4afb732e3995ca461236d7c5ab4eba107e3a4116
Ralph Smith <ralphsmith80@gmail.com>
2013-03-05 11:05:27 -0600

    make the puller script parallel processing

--- puller.sh
+++ puller.sh
@@ -3,13 +3,16 @@
 workspace=/Users/ralphs/Workspace
 
 for d in $workspace/*; do
+(
 	if [[ $d == $workspace/nvm || $d == $workspace/gitosis-admin ]]
 	then
 		# echo "skipping $d"
 		continue
 	fi
-	# ( [ -d $d ] && cd $d && echo "pulling $d" )
-    ( [ -d $d ] && cd $d && [ -d .git ] && echo -n "### pulling `basename "$d"` " && git pull )
-    ( [ -d $d ] && cd $d && [ -d .svn ] && echo "### updating `basename "$d"` " && svn up ) || true
+	# ( [ -d $d ] && cd $d && echo "pulling $d" ); sleep 2
+    ( [ -d $d ] && cd $d && [ -d .git ] && echo -ne "### pulling `basename "$d"` \n" && git pull )
+    ( [ -d $d ] && cd $d && [ -d .svn ] && echo -e "### updating `basename "$d"` \n" && svn up ) || true
+) &
 done
+wait
 echo 'done!!'
